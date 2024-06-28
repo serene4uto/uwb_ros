45dcb7527b2473ee44e2098d557a56d78f913d5b
HaTrung <nguyenhatrung411@gmail.com>
2024-06-28 07:52:13 +0000

    up

---
+++ py_uwb_localization/launch/tag_localizer.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    tag_localizer_node = Node(
+        package='py_uwb_localization',
+        executable='uwb_tag_localizer',
+        name='py_tag_localizer',
+        output='screen'
+    )
+
+    ekf_filter_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        parameters=[os.path.join(get_package_share_directory("py_uwb_localization"), 'config', 'uwb_ekf.yaml')],
+
+    )
+    
+    ld = LaunchDescription()
+
+    ld.add_action(tag_localizer_node)
+    ld.add_action(ekf_filter_node)
+
+    return ld
