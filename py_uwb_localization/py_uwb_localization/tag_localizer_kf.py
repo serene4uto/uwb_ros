c5e980da6bd5d1e1337b334266f7ae213dcad9ba
zeki <nguyenhatrung411@gmail.com>
2024-11-01 11:34:13 +0000

    Refine spike filtering in UWB tag localization and update RViz configuration for improved visualization

--- py_uwb_localization/py_uwb_localization/tag_localizer_kf.py
+++ py_uwb_localization/py_uwb_localization/tag_localizer_kf.py
@@ -222,14 +222,17 @@ class UwbTagLocalizer(Node):
 
         # eliminate spikes error
         displacement_xy = np.sqrt((point_msg.point.x - self.spikes_filtered_point_msg.point.x)**2 + (point_msg.point.y - self.spikes_filtered_point_msg.point.y)**2)
-        self.get_logger().info(f"Displacement: {displacement_xy}") 
+        # self.get_logger().info(f"Displacement: {displacement_xy}") 
 
         # self.spikes_filtered_point_msg = point_msg
         if displacement_xy < UPPER_REJECT_THRESHOLD and displacement_xy > LOWER_REJECT_THRESHOLD:
+            self.get_logger().info(f"Displacement: {displacement_xy}")
             self.spikes_filtered_point_msg = point_msg
 
         #Publish the new filtered position
+        self.spikes_filtered_point_msg.header.stamp = self.get_clock().now().to_msg() # Update the timestamp
         self.uwb_spikes_filtered_pub.publish(self.spikes_filtered_point_msg)
+        self.get_logger().info(f"Filtered position: {self.spikes_filtered_point_msg.point.x}, {self.spikes_filtered_point_msg.point.y}, {self.spikes_filtered_point_msg.point.z}")
 
 
 
