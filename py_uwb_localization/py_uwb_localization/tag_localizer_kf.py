bc0ef9c6944fc3a6dd863afd3c2fc84d7647d388
Zeki <nguyenhatrung411@gmail.com>
2024-11-16 01:52:04 +0000

    Update UWB anchor positions and increase upper rejection threshold for improved localization accuracy

--- py_uwb_localization/py_uwb_localization/tag_localizer_kf.py
+++ py_uwb_localization/py_uwb_localization/tag_localizer_kf.py
@@ -9,7 +9,7 @@ from scipy.optimize import least_squares
 import yaml
 import os
 
-UPPER_REJECT_THRESHOLD = 0.5 # meters, means if the distance between two points is greater than this meters, the new point will be rejected
+UPPER_REJECT_THRESHOLD = 0.8 # meters, means if the distance between two points is greater than this meters, the new point will be rejected
 LOWER_REJECT_THRESHOLD = 0.0 # meters, means if the distance between two points is less than this meters, the new point will be rejected
 
 def multilateration(anchors, distances):
